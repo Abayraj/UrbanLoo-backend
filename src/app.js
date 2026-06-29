e148f7bd3772a258945325281d3bea02586e7e3e
Abayraj <abayunni5@gmail.com>
2026-06-29 17:24:01 +0530

    add mongo db and development  server connection

--- src/app.js
+++ src/app.js
@@ -1,13 +1,27 @@
 const express = require('express');
+require('dotenv').config();
 const cors = require('cors');
+const connectDB = require('./config/db');
 
+const authRoutes = require('./routes/auth');
+const verifyJWT = require('./middlewares/verifyJWT');
+const testController = require('./controllers/testController');
 const app = express();
 
+const PORT = process.env.PORT || 5000;
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`🚀 Server running on port ${PORT}`);
+  });
+});
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
- console.log('Hello World');
+// Public routes
+app.use('/api/auth', authRoutes);
+app.get('/api/test', verifyJWT, (req, res) => {
+  res.json({ message: 'token is valid', user: req.user });
 });
 
 module.exports = app;
