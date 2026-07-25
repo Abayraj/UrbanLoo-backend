709cb7de49f468118401e8137a3d1f3d2f15d90a
Abayraj <abayunni5@gmail.com>
2026-07-25 13:36:09 +0530

    update jwt expire time

--- src/utils/token.js
+++ src/utils/token.js
@@ -3,12 +3,12 @@ const jwt = require('jsonwebtoken');
 const generateAccessToken = (userId) => {
   return jwt.sign(
     { id: userId },process.env.JWT_SECRET,
-    { expiresIn: '5s',}); // Change to 15m for production
+    { expiresIn: '30min',}); // Change to 15m for production
 };
 
 const generateRefreshToken = (userId) => {
   return jwt.sign({ id: userId }, process.env.REFRESH_SECRET,
-    {expiresIn: '2m',}); // Change to 30d for production
+    {expiresIn: '2days',}); // Change to 30d for production
 };
 
 module.exports = { generateAccessToken,generateRefreshToken,};
